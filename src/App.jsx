ef96dc118297f3f2d22f2b9da8e461d78cca0f06
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-02-11 23:31:36 +0600

    fav

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
-import { FavouriteProvider, WeatherProvider } from "./provider";
+import { FavouriteProvider, LocationProvider, WeatherProvider } from "./provider";
 
 export default function App() {
   return (
-    <WeatherProvider>
-      <FavouriteProvider>
-        <div className="grid place-items-center h-screen">
-          <Header />
-          <main className="mt-16">
-            <section>
-              <WeatherBoard />
-            </section>
-          </main>
-        </div>
-      </FavouriteProvider>
-    </WeatherProvider>
+    <LocationProvider>
+      <WeatherProvider>
+        <FavouriteProvider>
+          <div className="grid place-items-center h-screen">
+            <Header />
+            <main className="mt-16">
+              <section>
+                <WeatherBoard />
+              </section>
+            </main>
+          </div>
+        </FavouriteProvider>
+      </WeatherProvider>
+    </LocationProvider>
   );
 }

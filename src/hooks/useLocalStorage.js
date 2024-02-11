61d2156a71977d6a0a7d60adf934c92355a79c30
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-02-11 22:48:37 +0600

    add and remove fav

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,14 +1,15 @@
-const { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const useLocalStorage = (storageKey, defaultValue) => {
-    const [value, setValue] = useState(JSON.parse(localStorage.getItem(storageKey)) ?? defaultValue);
+    const [value, setValue] = useState(
+        JSON.parse(localStorage.getItem(storageKey)) ?? defaultValue
+    );
 
     useEffect(() => {
         localStorage.setItem(storageKey, JSON.stringify(value));
-    }, [value, storageKey])
+    }, [value, storageKey]);
 
     return [value, setValue];
-
-}
+};
 
 export default useLocalStorage;
